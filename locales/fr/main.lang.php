376a4f4049c5fc59ac7e5c005d5dde615bdcd154
Jean-Christian Denis <contact@jcdenis.fr>
2023-05-01 00:53:14 +0200

    update translation

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -1,33 +1,37 @@
 <?php
-/**
- * @package Dotclear
- *
- * @copyright Olivier Meunier & Association Dotclear
- * @copyright GPL-2.0-only
- */
+// Language: Français
+// Module: alias - 1.6
+// Date: 2023-04-30 22:52:13
+// Author: Jean-Christian Denis
+// Translated with Translater - 2023.04.23
+
+
 #
 # DOT NOT MODIFY THIS FILE !
 #
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Aliases']                             = 'Alias';
-L10n::$locales['Invalid aliases definitions']         = 'Définitions d\'alias invalides';
-L10n::$locales['Alias URL is empty.']                 = 'L\'URL de l\'alias est vide.';
-L10n::$locales['Alias destination is empty.']         = 'La destination de l\'alias est vide.';
-L10n::$locales['Aliases successfully updated.']       = 'Alias mis à jour avec succès.';
-L10n::$locales['Alias successfully created.']         = 'Alias créer avec succès.';
-L10n::$locales['New alias']                           = 'Nouvel alias';
-L10n::$locales['Alias URL:']                          = 'URL de l\'alias :';
-L10n::$locales['Alias destination:']                  = 'Destination de l\'alias :';
+L10n::$locales['Invalid aliases definitions'] = 'Définitions d\'alias invalides';
+L10n::$locales['Alias URL is empty.'] = 'L\'URL de l\'alias est vide.';
+L10n::$locales['Alias destination is empty.'] = 'La destination de l\'alias est vide.';
+L10n::$locales['Aliases successfully updated.'] = 'Alias mis à jour avec succès.';
+L10n::$locales['Alias successfully created.'] = 'Alias créer avec succès.';
+L10n::$locales['New alias'] = 'Nouvel alias';
+L10n::$locales['Alias URL:'] = 'URL de l\'alias :';
+L10n::$locales['Alias destination:'] = 'Destination de l\'alias :';
 L10n::$locales['Do not put blog URL "%s" in fields.'] = 'Ne pas mettre l\'URL du blog "%s" dans les champs.';
-L10n::$locales['No alias']                            = 'Aucun alias';
-L10n::$locales['There is %s alias.']                  = [
-    'Il y a %s alias.',
-    'Il y a %s alias.',
+L10n::$locales['Do visible redirection to destination'] = 'Faire une redirection visible vers la destination';
+L10n::$locales['No alias'] = 'Aucun alias';
+L10n::$locales['There is %s alias.'] = [
+	'Il y a %s alias.',
+	'Il y a %s alias.',
 ];
-L10n::$locales['Aliases list']                                      = 'Liste des alias';
-L10n::$locales['Alias URL']                                         = 'URL de l\'alias';
-L10n::$locales['Alias destination']                                 = 'Destination de l\'alias';
-L10n::$locales['Alias position']                                    = 'Position de l\'alias';
+L10n::$locales['Aliases list'] = 'Liste des alias';
+L10n::$locales['Alias URL'] = 'URL de l\'alias';
+L10n::$locales['Alias destination'] = 'Destination de l\'alias';
+L10n::$locales['Alias position'] = 'Position de l\'alias';
+L10n::$locales['Redrection'] = 'Redirection';
+L10n::$locales['visible redirection to %s'] = 'Redirection visible vers %s';
 L10n::$locales['To remove an alias, empty its URL or destination.'] = 'Pour supprimer un alias, videz son URL ou sa destination.';
+L10n::$locales['Aliases'] = 'Alias';
