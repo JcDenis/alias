254e275568dbb8f94ee61f04c048f88958f64e70
Jean-Christian Denis <contact@jcdenis.fr>
2025-03-02 09:51:22 +0100

    release 1.11.1

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     'alias',
     "Create aliases of your blog's URLs",
     'Olivier Meunier and contributors',
-    '1.11',
+    '1.11.1',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
